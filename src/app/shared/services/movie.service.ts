f773dff606ee42eb32e8ae697a939ef69afe6c05
Ale Ksandar <88aleksandar88@gmail.com>
2017-10-19 22:03:39 +0200

    treci pri kraju

--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import {movieList} from '../example';
+import { Observable, Observer } from 'rxjs';
+import { Movie } from '../models/movie';
 
 @Injectable()
 export class MovieService {
 
+	private movies:Array<Movie>;
+
   constructor() { }
+
+  public getMovies(){
+  	return new Observable((o: Observer<any>) => {
+  		this.movies = movieList.map(x => new Movie(x.id, x.name, x.director, x.imageUrl, x.duration, x.releaseDate, x.genres) );
+  		o.next(this.movies);
+  	});
+
+  }
 
 }
